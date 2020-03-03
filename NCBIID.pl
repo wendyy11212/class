af7f5fcc7fc67c5a073226b6b97f340f3b7e358e
wendy <wendyy11212@gmail.com>
2020-03-03 16:16:48 +0000

    PERL Scripts

--- NCBIID.pl
+++ NCBIID.pl
@@ -5,7 +5,7 @@ $gi_list = '24475906,224465210,50978625,9507198';
 
 #url
 $base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
-$url = $base . "efetch.fcgi?db=nucleaotide&id=$gi_list&rettype=acc";
+$url = $base . "efetch.fcgi?db=nucleotide&id=$gi_list&rettype=acc";
 
 #post
 $output = get($url);
